8c38c2b3a2438374d7747169a52cf30669dab6c1
Simon Oulevay <simon.oulevay@gmail.com>
2019-08-26 18:30:47 +0200

    Fix user agent date storage

--- src/app.service.ts
+++ src/app.service.ts
@@ -22,15 +22,31 @@ export class AppService {
       return [];
     }
 
-    const uas = await db.zrangebyscore(key('ua'), 0, Date.now());
-    await this.cleanUpUserAgents(uas.reverse().slice(memory));
+    const uas = await this.listUserAgents();
+    await this.cleanUpUserAgents(uas.slice(memory));
 
-    return uas.reverse().slice(0, memory).map(ua => JSON.parse(ua));
+    return uas.slice(0, memory).map(ua => ({
+      ...JSON.parse(ua.ua),
+      createdAt: new Date(ua.createdAt).toISOString()
+    }));
   }
 
-  private async cleanUpUserAgents(uas: string[]) {
+  private async cleanUpUserAgents(uas: Array<{ ua: string, createdAt: number }>) {
     if (uas.length) {
-      await db.zrem(key('ua'), uas);
+      await db.zrem(key('ua'), uas.map(ua => ua.ua));
     }
+  }
+
+  private async listUserAgents() {
+
+    const uasWithScores = await db.zrangebyscore(key('ua'), 0, Date.now(), 'WITHSCORES');
+    if (!uasWithScores.length) {
+      return [];
+    }
+
+    return Array(uasWithScores.length / 2).fill(0).map((_, i) => ({
+      ua: uasWithScores[i * 2],
+      createdAt: parseInt(uasWithScores[i * 2 + 1], 10)
+    })).reverse();
   }
 }
