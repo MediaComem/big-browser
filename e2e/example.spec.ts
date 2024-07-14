fad9cc54833b91ee19e985f71f70171b530184bc
Simon Oulevay <simon.oulevay@gmail.com>
2024-07-15 00:57:51 +0200

    Add playwright test

---
+++ e2e/example.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ playwright }) => {
+  const firefox = await playwright.firefox.launch();
+  const firefoxContext = await firefox.newContext();
+  const firefoxPage = await firefoxContext.newPage();
+  await firefoxPage.goto('/');
+  await expect(firefoxPage).toHaveTitle(/Big Browser/);
+
+  const chromium = await playwright.chromium.launch();
+  const chromiumContext = await chromium.newContext();
+  const chromiumPage = await chromiumContext.newPage();
+  await chromiumPage.goto('/');
+  await expect(chromiumPage).toHaveTitle(/Big Browser/);
+
+  const webkit = await playwright.webkit.launch();
+  const webkitContext = await webkit.newContext();
+  const webkitPage = await webkitContext.newPage();
+  await webkitPage.goto('/');
+  await expect(webkitPage).toHaveTitle(/Big Browser/);
+});
